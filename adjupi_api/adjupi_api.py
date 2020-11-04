14d861ae9d70f5e1bcf7cb995d32bb0f359486a0
Steffen Karcher <steffen.karcher@googlemail.com>
2020-11-04 15:56:32 +0100

    late commit, dont know what i did

--- adjupi_api/adjupi_api.py
+++ adjupi_api/adjupi_api.py
@@ -1,15 +1,13 @@
 from flask import Flask
 from flask_cors import CORS
 from flask_restplus import Api, Resource
-from mqtt_client import MqttClient
+import paho.mqtt.client as mqtt
 
 
 app = Flask(__name__)
 CORS(app)
 api = Api(app)
-swagger_api = api.namespace('homepi', description='OpenAPI for HomePi')
-
-client = MqttClient()
+swagger_api = api.namespace('adjupi', description='OpenAPI for Adjupi')
 
 
 @swagger_api.route('/')
@@ -31,9 +29,14 @@ class SwitchRoute(Resource):
 
     def get(self, command, id):
         """sets switch on or off and return success or failure"""
+        client = mqtt.Client()
+        client.connect(BROKER_ADDRESS)
         print(f"Route '/switch_{command}' has been requested.")
         client.publish(f'zigbee2mqtt/{id}/set/state', command.upper())
         return {'status': 'test successful'}
 
+
 if __name__ == '__main__':
+    BROKER_ADDRESS = "localhost"
+
     app.run(host='0.0.0.0', debug=False)  # starts development server
